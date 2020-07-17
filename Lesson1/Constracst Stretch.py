26ae9c45966100f9709c417e46dda3eddd29b92d
khanhhoaa19 <>
2020-07-17 08:18:27 +0700

    new

---
+++ Lesson1/Constracst Stretch.py
@@ -0,0 +1,5 @@
+import cv2 as cv
+import numpy as np
+img = cv.imread('D:\\q.JPG')
+cv.imshow("Display", img)
+cv.destroyAllWindows()
---
+++ Lesson1/LuminanceHistogram.py
@@ -0,0 +1,11 @@
+# from PIL import Image
+# from math import sqrt
+# img = Image.open("D:\\abcd.JPG")
+# img = img.convert('RGB')
+# x = 0
+# y = 0
+# pixelRGB = img.getpixel((x,y))
+# R,G,B = pixelRGB
+# print(R, G, B)
+# Luminance = 0.299*R + 0.587*G + 0.114*B
+# print(Luminance)
